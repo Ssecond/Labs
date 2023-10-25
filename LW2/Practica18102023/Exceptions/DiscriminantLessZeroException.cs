8d64329769ca4e3f78ce8497a4bf7b65c8135b90
Second <artemtrushnikov@mail.ru>
2023-10-25 19:18:04 +1000

    Second lab added

---
+++ LW2/Practica18102023/Exceptions/DiscriminantLessZeroException.cs
@@ -0,0 +1,8 @@
+﻿namespace Practica18102023.Exceptions
+{
+    internal class DiscriminantLessZeroException : Exception
+    {
+        internal DiscriminantLessZeroException() { }
+        internal DiscriminantLessZeroException(string? message) : base(message) { }
+    }
+}
