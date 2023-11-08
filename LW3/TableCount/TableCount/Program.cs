b52d6fc07fe0723a69535ad65673193ba94da291
Ssecond <artemtrushnikov@mail.ru>
2023-11-08 11:19:44 +1000

    Added overall structure of LW3

---
+++ LW3/TableCount/TableCount/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TableCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int Calculate(5, 5, (x, y) => x * y);
+        }
+        delegate int Do(int x, int y);
+        static int[,] Calculate(int x, int y, Do func)
+        {
+            int[,] table = new int[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    table[x, y] = i * j;
+                }
+            }
+        }
+    }
+}
