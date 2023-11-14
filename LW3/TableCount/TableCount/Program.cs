bced3011c70ec1d852717076caa7ed3666f98524
Second <artemtrushnikov@mail.ru>
2023-11-14 20:29:13 +1000

    LW3 done

--- LW3/TableCount/TableCount/Program.cs
+++ LW3/TableCount/TableCount/Program.cs
@@ -4,20 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            int Calculate(5, 5, (x, y) => x * y);
+            int[,] table = Calculate(5, 5, (x, y) => x * y);
+            PrintTable(table);
         }
-        delegate int Do(int x, int y);
-        static int[,] Calculate(int x, int y, Do func)
+        private static void PrintTable(int[,] table, int countDigits = 2)
+        {
+            string numberOutFormat = $"{{0,{countDigits}}} ";
+            // Print top number row
+            Console.Write(new string(' ', countDigits + 1));
+            for (int j = 0; j < table.GetLength(1); j++)
+                Console.Write(numberOutFormat, j + 1);
+            Console.WriteLine();
+
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                Console.Write(numberOutFormat, i + 1); // Print left number column
+                for (int j = 0; j < table.GetLength(1); j++)
+                    Console.Write(numberOutFormat, table[i, j]);
+                Console.WriteLine();
+            }
+        }
+
+        delegate int MathAction(int x, int y);
+        private static int[,] Calculate(int x, int y, MathAction action)
         {
             int[,] table = new int[x, y];
             for (int i = 0; i < x; i++)
-            {
                 for (int j = 0; j < y; j++)
-                {
-                    table[x, y] = i * j;
-                }
-            }
+                    table[i, j] = action(i + 1, j + 1);
+            return table;
         }
     }
 }
