fff0e92070757b5ae353db573e3d74c6dfbb8de7
Second <artemtrushnikov@mail.ru>
2023-11-28 22:12:29 +1000

    Логирование в XML готово.

--- LW4/Practica18102023/ErrorLog.cs
+++ LW4/Practica18102023/ErrorLog.cs
@@ -1,11 +1,13 @@
 ﻿using System.Text;
-using System.Xml;
+using System.Xml.Linq;
 
 namespace Practica18102023
 {
     internal class ErrorLog
     {
-        const string LOG_NAME_ON_DEFAULT = "log";
+        private const string LOG_NAME_ON_DEFAULT = "log";
+        private const string DATA_TIME_FILE_FORMAT = "dd.MM.yyyy HH_mm_ss";
+
         private static ErrorLog? instance;
         protected ErrorLog() { }
         internal static ErrorLog Initialize()
@@ -20,10 +22,10 @@ namespace Practica18102023
         {
             try
             {
-
+                DateTime currentTime = DateTime.Now;
                 if (streamWriter == null)
                 {
-                    logTXTName += $"{fileName} {DateTime.Now.ToString("dd.MM.yyyy HH_mm_ss")}";
+                    logTXTName += $"{fileName} {currentTime.ToString(DATA_TIME_FILE_FORMAT)}";
                     streamWriter = new StreamWriter(logTXTName + ".txt", false);
                     streamWriter.WriteLine(GetSystemInfo());
                 }
@@ -31,7 +33,7 @@ namespace Practica18102023
                     streamWriter = new StreamWriter(logTXTName + ".txt", true);
 
 
-                streamWriter.WriteLine($"[{DateTime.Now}, UTC {TimeZoneInfo.Local.GetUtcOffset(DateTime.Now).Hours.ToString("+#;-#;0")}] {exception.Message}");
+                streamWriter.WriteLine($"[{currentTime}, UTC {TimeZoneInfo.Local.GetUtcOffset(currentTime).Hours.ToString("+#;-#;0")}] {exception.Message}");
                 streamWriter.WriteLine("Stack trace: " + exception.StackTrace);
             }
             catch (Exception e)
@@ -46,19 +48,48 @@ namespace Practica18102023
             }
         }
 
+        private XDocument xmlDoc = null;
+        private string logXMLName;
         internal void LogXML(Exception exception, string fileName = LOG_NAME_ON_DEFAULT)
         {
             try
             {
+                DateTime currentTime = DateTime.Now;
+                XElement log = new XElement("log");
+                if (xmlDoc == null)
+                {
+                    xmlDoc = new XDocument();
+                    logXMLName += $"{fileName} {currentTime.ToString(DATA_TIME_FILE_FORMAT)}";
+
+                    XElement systemInfo = new XElement("system_info");
+                    systemInfo.Add(GetSystemInfo());
+                    log.Add(systemInfo);
+                }
+
+                XElement error = new XElement("error");
+
+                    XElement time = new XElement("time");
+                    time.Add(currentTime.ToString());
+
+                    XElement timeZone = new XElement("UTC");
+                    timeZone.Add(TimeZoneInfo.Local.GetUtcOffset(DateTime.Now).Hours.ToString("+#;-#;0"));
+
+                    XElement errorMessage = new XElement("message");
+                    errorMessage.Add(exception.Message);
+
+                error.Add(time);
+                error.Add(timeZone);
+                error.Add(errorMessage);
+
+                log.Add(error);
+                xmlDoc.Add(log);
+                xmlDoc.Save(logXMLName + ".xml");
             }
             catch (Exception e)
             {
                 Console.WriteLine("В процессе логирования ошибки произошла непредвиденная ошибка :(");
                 Console.WriteLine("Message: " + e.Message);
                 Console.WriteLine("Stack trace: " + exception.StackTrace);
-            }
-            finally
-            {
             }
         }
         internal void LogJSON(Exception exception, string fileName = LOG_NAME_ON_DEFAULT)
@@ -82,7 +113,6 @@ namespace Practica18102023
                 stringBuilder.AppendLine($"Формат диска: {dI.DriveFormat}");
                 stringBuilder.AppendLine($"Размер диска (ГБ): {diskSize}");
                 stringBuilder.AppendLine($"Доступное свободное место (ГБ): {diskFreeSpace}");
-                stringBuilder.AppendLine();
             }
             return stringBuilder.ToString();
         }
