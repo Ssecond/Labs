b0cc81acc48a452d3590e9901bdf3dc6acff75ef
Second <artemtrushnikov@mail.ru>
2023-12-04 19:20:06 +1000

    Добавлен тест инициализации для калькулятора.

--- LW6/CalculatorLib/CalculatorTest/UnitTest1.cs
+++ LW6/CalculatorLib/CalculatorTest/UnitTest1.cs
@@ -5,13 +5,20 @@ namespace CalculatorTest
     [TestClass]
     public class UnitTest1
     {
+        int a, b;
+        [TestMethod]
+        [TestInitialize]
+        public void Initialize()
+        {
+            Random rand = new Random();
+            a = rand.Next(int.MinValue, int.MaxValue);
+            b = rand.Next(int.MinValue, int.MaxValue);
+        }
         [TestMethod]
         public void TestSumRandom()
         {
             Calculator calculator = new Calculator();
             Random rand = new Random();
-            int a = rand.Next(int.MinValue, int.MaxValue);
-            int b = rand.Next(int.MinValue, int.MaxValue);
             int expend = a + b;
             int actual = calculator.Sum(a, b);
             Assert.AreEqual(expend, actual);
@@ -21,8 +28,6 @@ namespace CalculatorTest
         {
             Calculator calculator = new Calculator();
             Random rand = new Random();
-            int a = rand.Next(int.MinValue, int.MaxValue);
-            int b = rand.Next(int.MinValue, int.MaxValue);
             int expend = a * b;
             int actual = calculator.Multiply(a, b);
             Assert.AreEqual(expend, actual);
@@ -32,9 +37,7 @@ namespace CalculatorTest
         {
             Calculator calculator = new Calculator();
             Random rand = new Random();
-            double a = rand.Next(int.MinValue, int.MaxValue);
-            double b = rand.Next(int.MinValue, int.MaxValue);
-            double expend = a / b;
+            double expend = (double)a / (double)b;
             double actual = calculator.Divide(a, b);
             Assert.AreEqual(expend, actual);
         }
@@ -43,8 +46,6 @@ namespace CalculatorTest
         {
             Calculator calculator = new Calculator();
             Random rand = new Random();
-            int a = rand.Next(int.MinValue, int.MaxValue);
-            int b = rand.Next(int.MinValue, int.MaxValue);
             int expend = a - b;
             int actual = calculator.Substuct(a, b);
             Assert.AreEqual(expend, actual);
@@ -56,8 +57,7 @@ namespace CalculatorTest
         {
             Calculator calculator = new Calculator();
             Random rand = new Random();
-            double a = rand.Next(int.MinValue, int.MaxValue);
-            double b = 0;
+            b = 0;
             calculator.Divide(a, b);
         }
     }
