6a5f5712965548b760db25103fa8498569174403
Second <artemtrushnikov@mail.ru>
2024-02-12 21:22:15 +1000

    ЛР прошлого семестра теперь в папке LastTerm. Добавлена ЛР1, сортировка пузырьком, слиянием и логирование.

---
+++ LW1/LW1/Logger.cs
@@ -0,0 +1,38 @@
+﻿namespace LW1
+{
+    internal class Logger
+    {
+        private const string LOG_NAME_ON_DEFAULT = "log";
+        private const string DATA_TIME_FILE_FORMAT = "dd.MM.yyyy HH_mm_ss";
+
+        private StreamWriter? streamWriter = null;
+        private string? logTXTName;
+        internal void Log(string text, string fileName = LOG_NAME_ON_DEFAULT)
+        {
+            try
+            {
+                DateTime currentTime = DateTime.Now;
+                if (streamWriter == null)
+                {
+                    logTXTName += $"{fileName} {currentTime.ToString(DATA_TIME_FILE_FORMAT)}";
+                    streamWriter = new StreamWriter(logTXTName + ".txt", false);
+                    streamWriter.WriteLine("Название приложения: " + AppDomain.CurrentDomain.FriendlyName);
+                }
+                else
+                    streamWriter = new StreamWriter(logTXTName + ".txt", true);
+
+                streamWriter.WriteLine($"[{currentTime}, UTC {TimeZoneInfo.Local.GetUtcOffset(currentTime).Hours.ToString("+#;-#;0")}] {text}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("В процессе логирования ошибки произошла непредвиденная ошибка :(");
+                Console.WriteLine("Message: " + e.Message);
+                Console.WriteLine("Stack trace: " + e.StackTrace);
+            }
+            finally
+            {
+                streamWriter?.Close();
+            }
+        }
+    }
+}
---
+++ LW1/LW1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            int[] testArray = { 5435, -43, 342, 0 };
+            BubleSort(testArray);
+            string buff = string.Empty;
+            foreach (int i in testArray)
+                buff += i + " ";
+            logger.Log(buff);
+        }
+        static void BubleSort(int[] nums)
+        {
+            for (int write = 0; write < nums.Length; write++)
+                for (int sort = 0; sort < nums.Length - 1; sort++)
+                    if (nums[sort] > nums[sort + 1])
+                        Swap(ref nums[sort + 1], ref nums[sort]);
+        }
+        static void Swap(ref int firstVar, ref int secondVar)
+        {
+            int buff = firstVar;
+            firstVar = secondVar;
+            secondVar = buff;
+        }
+        static void MergeSortImpl(int[] nums, int[] buffer, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+                MergeSortImpl(nums, buffer, l, m);
+                MergeSortImpl(nums, buffer, m + 1, r);
+
+                int k = l;
+                for (int i = l, j = m + 1; i <= m || j <= r;)
+                {
+                    if (j > r || (i <= m && nums[i] < nums[j]))
+                    {
+                        buffer[k] = nums[i];
+                        ++i;
+                    }
+                    else
+                    {
+                        buffer[k] = nums[j];
+                        ++j;
+                    }
+                    ++k;
+                }
+                for (int i = l; i <= r; ++i)
+                    nums[i] = buffer[i];
+            }
+        }
+
+        static void MergeSort(int[] nums)
+        {
+            if (nums.Length != 0)
+            {
+                int[] buffer = new int[nums.Length];
+                MergeSortImpl(nums, buffer, 0, nums.Length - 1);
+            }
+        }
+    }
+}
