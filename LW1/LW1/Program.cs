773363b2932b28655bfb06acfb4c1a3cc368e3c7
Ssecond <artemtrushnikov@mail.ru>
2024-02-14 09:35:24 +1000

    Добавил сортировку бинарным деревом.

--- LW1/LW1/Program.cs
+++ LW1/LW1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LW1
 {
@@ -9,18 +11,17 @@ namespace LW1
         static void Main(string[] args)
         {
             int[] testArray;
-            RandomFillArray(out testArray, 10_000);
+            RandomFillArray(out testArray, 1_000);
             logger.Log("Original array." + "\n\t" + ArrayToPrintString(testArray));
-            BubleSort(testArray);
+            BinaryTreeSort(testArray);
+            logger.Log("Sorted array." + "\n\t" + ArrayToPrintString(testArray));
         }
         static void RandomFillArray(out int[] nums, long length)
-        { 
+        {
             nums = new int[length];
             Random random = new Random();
             for (long i = 0; i < length; i++)
-            {
                 nums[i] = random.Next();
-            }
         }
         static string ArrayToPrintString(int[] nums, long startIndex = 0, long endIndex = -1)
         {
@@ -29,8 +30,9 @@ namespace LW1
                 endIndex = nums.LongLength - 1;
 
             for (long i = startIndex; i < endIndex; i++)
-                sb.Append(nums[i] + ", ");
-            sb.Append(nums[nums.LongLength - 1] + ".");
+                sb.Append(string.Format("{0:n0}", nums[i]) + ", ");
+            sb.Append(string.Format("{0:n0}", nums[nums.LongLength - 1]) + ".");
+
             return sb.ToString();
         }
         static void BubleSort(int[] nums)
@@ -90,5 +92,64 @@ namespace LW1
             MergeSortImpl(nums, buffer, 0, nums.Length - 1);
             logger.Log($"Merge sort's done. Time spent: {timeCountner.ElapsedMilliseconds} milliseconds.");
         }
+
+        public static void BinaryTreeSort(int[] nums)
+        {
+            Stopwatch timeCountner = Stopwatch.StartNew();
+            TreeNode treeNode = new TreeNode(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+                treeNode.Insert(new TreeNode(nums[i]));
+
+            logger.Log($"Binary tree sort's done. Time spent: {timeCountner.ElapsedMilliseconds} milliseconds.");
+            nums = treeNode.Transform();
+        }
+    }
+    public class TreeNode
+    {
+
+        public TreeNode(int data)
+        {
+            Data = data;
+        }
+
+        public int Data { get; set; }
+
+        public TreeNode Left { get; set; }
+
+        public TreeNode Right { get; set; }
+
+        public void Insert(TreeNode node)
+        {
+            if (node.Data < Data)
+            {
+                if (Left == null)
+                    Left = node;
+                else
+                    Left.Insert(node);
+            }
+            else
+            {
+                if (Right == null)
+                    Right = node;
+                else
+                    Right.Insert(node);
+            }
+        }
+        public int[] Transform(List<int>? elements = null)
+        {
+            if (elements == null)
+                elements = new List<int>();
+
+            if (Left != null)
+                Left.Transform(elements);
+
+            elements.Add(Data);
+
+            if (Right != null)
+                Right.Transform(elements);
+
+            return elements.ToArray();
+        }
+
     }
 }
